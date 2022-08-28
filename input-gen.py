914609e924f34401621c70058eb901fa0dccecba
AnnaAD <annaad@mit.edu>
2022-08-28 18:32:35 -0400

    cleanup

--- input-gen.py
+++ input-gen.py
@@ -7,9 +7,9 @@ seen = set()
 for i in range(10000000):
     num1 = random.randint(0,vertices)
     num2 = random.randint(0,vertices)
-    if(num1 != num2 and (str(num1) + "," + str(num2)) not in seen and (str(num2) + "," + str(num1)) not in seen):
-        seen.add(str(num1) + "," + str(num2))
-        output += str(num1) + "," + str(num2) + "\n"
+    if(num1 != num2 and (str(num1) + "	" + str(num2)) not in seen and (str(num2) + "	" + str(num1)) not in seen):
+        seen.add(str(num1) + "	" + str(num2))
+        output += str(num1) + "	" + str(num2) + "\n"
 
 f = open("input.txt", "w")
 f.write(output)
